a6dd7c85adcc060cbf845448ce960fdc3befde26
PaigeN2026 <Paige.nunez2026@gbwl.org>
2025-04-01 12:44:49 +0000

    4/1

--- Main.java
+++ Main.java
@@ -1,6 +1,16 @@
 public class Main {
 
    public static void main(String []args) {
-      System.out.println("Hello World");
+      System.out.println("Recursive Methods");
+      // neverEnd(); causes a StackOverflow error because INFINITE LOOP
    }
+
+   // RECURSIVE methods call themselves (to repeat code) 
+   // to INDENTIFY a recursive method, look for its name again in the curly brackets
+   public static void neverEnd() {
+      System.out.println("Recursion is cool");
+      // Contains at least ONE CALL to itself in the METHOD BODY
+      neverEnd();
+   }
+
 }
