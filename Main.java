f5f0ee6d64620ed04f689a0852263c3373035c06
PaigeN2026 <Paige.nunez2026@gbwl.org>
2025-04-02 13:27:56 +0000

    4/2

--- Main.java
+++ Main.java
@@ -3,6 +3,11 @@ public class Main {
    public static void main(String []args) {
       System.out.println("Recursive Methods");
       // neverEnd(); causes a StackOverflow error because INFINITE LOOP
+
+      // Test the factorial method
+      System.out.println(factorial(5));
+      System.out.println(factorial(4));
+      System.out.println(factorial(31)); // LIMIT for integer data type
    }
 
    // RECURSIVE methods call themselves (to repeat code) 
@@ -11,6 +16,18 @@ public class Main {
       System.out.println("Recursion is cool");
       // Contains at least ONE CALL to itself in the METHOD BODY
       neverEnd();
+   }
+
+   // Recursive methods need a BASE CASE to stop
+   public static int factorial(int n) {
+      // BASE CASE condition: stop at 1
+      if (n == 1) {
+         return 1;
+      }
+      // Otherwise, return a RECURSIVE call to continue
+      else {
+         return n * factorial(n-1);
+      }
    }
 
 }
